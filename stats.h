6476988c77dc10b8469453086d263d7191492318
AbhiPalankar <105204004+AbhiPalankar@users.noreply.github.com>
2022-05-17 22:50:58 +0530

    Update stats.h
    updated structure member

--- stats.h
+++ stats.h
@@ -2,6 +2,9 @@
 #define STATS_H
 
 struct Stats compute_statistics(const float* numberset, int setlength);
+struct Stats{
+  float average;
+};
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
